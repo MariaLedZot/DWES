2d67e15a8352ec37c7400c35fadfb1225cd4f78a
MariaLedZot <mledzot325@g.educaand.es>
2025-10-03 18:12:12 +0200

    Actividad 2, ejercicio 8 (sin terminar).

---
+++ www/unidad2/act2/act2_8.php
@@ -0,0 +1,30 @@
+<?php
+/*
+---
+title: Ejercicio 8: Operador Ternario.
+desc: Otro uso del operador ternario.
+tags: [PHP, basico, operadores, ternario]
+code: https://github.com/
+---
+*/
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="author" content="María Ledesma Zotano • 2º DAW">
+    <title>Ejercicio 8</title>
+</head>
+
+<body>
+    <?php
+    $precio1 = 25;
+    $precio2 = 30;
+
+    ?>
+</body>
+
+</html>
